8ae82a2153cca303d1a48d56dc8ad70515f7a098
Dmitry Sirotkin <dsirotkin@servicechannel.com>
2017-11-04 01:26:36 +0300

    Two templates were just added.

---
+++ EventBuilder/Controllers/EventsController.cs
@@ -0,0 +1,28 @@
+﻿namespace EventBuilder.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using System.Collections.Generic;
+
+    [Route("events")]
+    public class EventsController : Controller
+    {
+        private readonly Dictionary<int, string> _templates;
+
+        public EventsController()
+        {
+            _templates = new Dictionary<int, string>();
+            _templates.Add(1, "BigOcean");
+            _templates.Add(2, "AliceLonely");
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Index(int id)
+        {
+            if (_templates.ContainsKey(id))
+            {
+                return View($"Templates/{_templates[id]}/Index.cshtml");
+            }
+            return View();
+        }
+    }
+}
